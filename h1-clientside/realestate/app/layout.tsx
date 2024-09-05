560bc688dedc2a0061645772a60de88e27e5530c
Patrick Jakobsen <97399891+Pallepadehat@users.noreply.github.com>
2024-09-05 23:20:13 +0200

    Fixed toaster

--- h1-clientside/realestate/app/layout.tsx
+++ h1-clientside/realestate/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/providers/providers'
 import { ThemeProvider } from '@/components/theme-provider'
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
